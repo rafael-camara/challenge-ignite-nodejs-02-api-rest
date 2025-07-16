a547a7701363fc60eecc5680adda98d2324c7d9a
rafael-camara <rafaelcamara.377@gmail.com>
2025-07-16 09:30:22 -0300

    feat:initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  return { hello: 'world' }
+})
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
