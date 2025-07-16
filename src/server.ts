7e5f910a348a5ac2c38f408125184993f539ff75
rafael-camara <rafaelcamara.377@gmail.com>
2025-07-16 10:14:41 -0300

    feat:config knex and creating first migration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  return { hello: 'world' }
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
